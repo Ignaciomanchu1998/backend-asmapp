4e09a128e8e03c3cd0faeb1bab189c1386c566ea
Ignacio Manchu <ignaciomanchu1998@gmail.com>
2022-05-01 22:16:56 -0500

    Avances en cotroller y dao

--- BackEnd/Controllers/UsuarioController.cs
+++ BackEnd/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BackEnd.Dao;
+using BackEnd.Models;
+using BackEnd.Settings.Globals;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackEnd.Controllers
@@ -7,5 +9,45 @@ namespace BackEnd.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private StructureResponse _struct = new();
+        private UsuarioDao _cant = new();
+
+        [HttpGet]
+        [Route("UsuarioGetAll")]
+        public async Task<IActionResult> UsuarioGetAll()
+        {
+            _struct = await _cant.UsuarioGetAll();
+            return Ok(_struct);
+        }
+
+        [HttpGet]
+        [Route("UsuarioGetById")]
+        public async Task<IActionResult> UsuarioGetById(int idUsuario)
+        {
+            _struct = await _cant.UsuarioGetById(idUsuario);
+            return Ok(_struct);
+        }
+       
+
+        [HttpPost]
+        public async Task<IActionResult> UsuarioPost([FromBody] UsuarioModel data)
+        {
+            _struct = await _cant.UsuarioPost(data);
+            return Ok(_struct);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UsuarioPut([FromBody] UsuarioModel data)
+        {
+            _struct = await _cant.UsuarioPut(data);
+            return Ok(_struct);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> UsuarioDelete(int idUsuario)
+        {
+            _struct = await _cant.UsuarioDelete(idUsuario);
+            return Ok(_struct);
+        }
     }
 }
